68cbbd020c52f87922c6021e1f33103ba111b4c0
chathuranga <dev.chathuranga@gmail.com>
2023-12-07 14:27:43 +0530

    not found prompt update

--- src/components/atomic/notFound.tsx
+++ src/components/atomic/notFound.tsx
@@ -1,8 +1,14 @@
 "use client";
-import { Box} from "@mui/material";
+import { Box, Typography} from "@mui/material";
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function NotFound() {
   return (
-      <Box sx={{width:'100%',p:3,fontSize:18}}>No Service found..!</Box>
+      <Box sx={{width:'100%',p:3,fontSize:18 ,border:'1px solid #e3e3e3',borderRadius:'6px',textAlign:'center'}}>
+        <SearchIcon sx={{fontSize:30}}/>
+        <Typography>
+          No Results Found
+          </Typography>
+        </Box>
   );
 }
